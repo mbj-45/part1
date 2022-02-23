373b111a654ba27c8e9d7e0bc8b1e94deb08cd52
BriceBOKATOLA <91880016+BriceBOKATOLA@users.noreply.github.com>
2022-02-23 09:14:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
             $filetype = $_FILES["photo"]["type"];
             $filesize = $_FILES["photo"]["size"];
 
-            // $ext = pathinfo($filename, PATHINFO_EXTENSION);
-            // if(!array_key_exists($ext, $allowed)) {
-            //     echo "Erreur : Veuillez sélectionner un format de fichier valide.";
-            // }
-            // $maxsize = 1024 * 1024;
-            // if($filesize > $maxsize) {
-            //     echo "Error: La taille du fichier est supérieure à la limite autorisée.";
-            // }
+            $ext = pathinfo($filename, PATHINFO_EXTENSION);
+            if(!array_key_exists($ext, $allowed)) {
+                echo "Erreur : Veuillez sélectionner un format de fichier valide.";
+            }
+            $maxsize = 1024 * 1024;
+            if($filesize > $maxsize) {
+                echo "Error: La taille du fichier est supérieure à la limite autorisée.";
+            }
         }
     }
 ?>
@@ -25,8 +25,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Document</title>
-<script src="./assets/script.js" async></script>
-<link href="./assets/style.css" rel="stylesheet">
+<script src="assets/script.js" async></script>
+<link href="assets/style.css" rel="stylesheet">
 </head>
 <body>
 <label for="fileToUpload">veuillez choisir une image :</label>
