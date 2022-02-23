6dd4719b2e84b134121e2ac277c0fa0a4e902bda
mbj-45 <92012408+mbj-45@users.noreply.github.com>
2022-02-23 09:50:51 +0100

    ;;ss
    question 1 to 4

--- index.php
+++ index.php
@@ -1,39 +1,43 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
-            $allowed = array("jpg" => "image/jpg", "png" => "image/png");
-            $filename = $_FILES["photo"]["name"];
-            $filetype = $_FILES["photo"]["type"];
-            $filesize = $_FILES["photo"]["size"];
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-            $ext = pathinfo($filename, PATHINFO_EXTENSION);
-            if(!array_key_exists($ext, $allowed)) {
-                echo "Erreur : Veuillez sélectionner un format de fichier valide.";
-            }
-            $maxsize = 1024 * 1024;
-            if($filesize > $maxsize) {
-                echo "Error: La taille du fichier est supérieure à la limite autorisée.";
-            }
-        }
+
+    if ($_FILES["fileToUpload"]["size"] > 1000000) {
+    echo "Désolé votre fichier doit faire moins de 1 Mo.</br>";
+    $uploadOk = 0;
+    } else if($imageFileType != "jpg" && $imageFileType != "png") {
+    echo "Votre fichier n'est pas une image.</br>";
+    $uploadOk = 0;
+    } if ($uploadOk == 0) {
+        echo "Votre fichier n'a pas été Upload.</br>";
+    } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "Le fichier". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " a bien été Upload.</br>";
+    } else {
+        echo "Une erreur s'est produite lors du téléchargement de votre fichier.</br>";
+    }
     }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
-<script src="assets/script.js" async></script>
-<link href="assets/style.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enregistrement d'images</title>
+    <script src="./assets/script.js" async></script>
+    <link href="./assets/style.css" rel="stylesheet">
 </head>
 <body>
-<label for="fileToUpload">veuillez choisir une image :</label>
+    <label for="fileToUpload">veuillez choisir une image :</label>
     <img id="imgPreview">
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="fileToUpload" id="fileToUpload"><br>
-        <button type="submit">Upload</button>
+    <form action="index.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="fileToUpload" id="fileToUpload"></br>
+        <input type="submit" value="Upload Image" name="submit"></br>
     </form>
-    </body>
+</body>
 </html>
